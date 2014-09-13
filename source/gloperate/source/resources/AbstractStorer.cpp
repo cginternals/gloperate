59ea60b2b372f9dd38a6ba7a82651d19cb6a99d8
Willy Scheibel <willyscheibel@gmx.de>
2014-09-13 18:10:43 +0200

    Add abstract storers

---
+++ source/gloperate/source/resources/AbstractStorer.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************\
+ * gloperate
+ *
+ * Copyright (C) 2014 Computer Graphics Systems Group at the 
+ * Hasso-Plattner-Institut (HPI), Potsdam, Germany.
+\******************************************************************************/
+#include <gloperate/resources/AbstractStorer.h>
+ 
+
+namespace gloperate
+{
+
+
+/**
+*  @brief
+*    Constructor
+*/
+AbstractStorer::AbstractStorer()
+{
+}
+
+/**
+*  @brief
+*    Destructor
+*/
+AbstractStorer::~AbstractStorer()
+{
+}
+
+
+} // namespace gloperate
