f9fa8dccb727a3dc2df6148ed14caa55bcb0cea4
Stefan Buschmann <stefan.buschmann@hpi.uni-potsdam.de>
2014-09-13 12:23:04 +0200

    Transform plugin registration into macros to avoid unnecessary code duplication

--- source/examples/BasicExamples/source/plugin.cpp
+++ source/examples/BasicExamples/source/plugin.cpp
@@ -1,54 +1,27 @@
-#include <vector>
 #include <gloperate/plugin/plugin_api.h>
-#include <gloperate/plugin/PainterPlugin.h>
 #include <basic-examples/SimpleTexture/SimpleTexture.h>
 #include <basic-examples/RotatingQuad/RotatingQuad.h>
 #include <basic-examples/CubeScape/CubeScape.h>
 
 
-static std::vector<gloperate::Plugin *> g_plugins;
+GLOPERATE_PLUGIN_LIBRARY
 
+    GLOPERATE_PLUGIN(SimpleTexture
+    , "SimpleTexture"
+    , "Displays a simple generated texture on the screen"
+    , "gloperate team"
+    , "v1.0.0" )
 
-extern "C" GLOPERATE_PLUGIN_API void initPluginLibrary()
-{
-    g_plugins.push_back(new gloperate::PainterPlugin<SimpleTexture>(
-                            "SimpleTexture"
-                          , "Displays a simple generated texture on the screen"
-                          , "gloperate team"
-                          , "v1.0.0" ) );
+    GLOPERATE_PLUGIN(RotatingQuad
+    , "RotatingQuad"
+    , "Displays a rotating quad geometry, demonstrating animation and cameras"
+    , "gloperate team"
+    , "v1.0.0" )
 
-    g_plugins.push_back(new gloperate::PainterPlugin<RotatingQuad>(
-                            "RotatingQuad"
-                          , "Displays a rotating quad geometry, demonstrating animation and cameras"
-                          , "gloperate team"
-                          , "v1.0.0" ) );
+    GLOPERATE_PLUGIN(CubeScape
+    , "CubeScape"
+    , "Displays a CubeScape"
+    , "gloperate team"
+    , "v1.0.0" )
 
-    g_plugins.push_back(new gloperate::PainterPlugin<CubeScape>(
-                            "CubeScape"
-                          , "Displays a CubeScape"
-                          , "gloperate team"
-                          , "v1.0.0" ) );
-}
-
-extern "C" GLOPERATE_PLUGIN_API int getNumOfPlugins()
-{
-    return (int)g_plugins.size();
-}
-
-extern "C" GLOPERATE_PLUGIN_API gloperate::Plugin * getPlugin(unsigned int index)
-{
-    if (index < (unsigned int)g_plugins.size()) {
-        return g_plugins[index];
-    } else {
-        return nullptr;
-    }
-}
-
-extern "C" GLOPERATE_PLUGIN_API void deinitPluginLibrary()
-{
-    for (std::vector<gloperate::Plugin *>::iterator it = g_plugins.begin(); it != g_plugins.end(); ++it) {
-        gloperate::Plugin * plugin = *it;
-        delete plugin;
-    }
-    g_plugins.clear();
-}
+GLOPERATE_PLUGIN_LIBRARY_END
--- source/examples/ExtendedExamples/source/plugin.cpp
+++ source/examples/ExtendedExamples/source/plugin.cpp
@@ -1,40 +1,13 @@
-#include <vector>
 #include <gloperate/plugin/plugin_api.h>
-#include <gloperate/plugin/PainterPlugin.h>
 #include <extended-examples/ExtendedCubeScape/ExtendedCubeScape.h>
 
 
-static std::vector<gloperate::Plugin *> g_plugins;
+GLOPERATE_PLUGIN_LIBRARY
 
+    GLOPERATE_PLUGIN(ExtendedCubeScape
+    , "ExtendedCubeScape"
+    , "Displays a CubeScape"
+    , "gloperate team"
+    , "v1.0.0" )
 
-extern "C" GLOPERATE_PLUGIN_API void initPluginLibrary()
-{
-    g_plugins.push_back(new gloperate::PainterPlugin<ExtendedCubeScape>(
-                            "ExtendedCubeScape"
-                          , "Displays a CubeScape"
-                          , "gloperate team"
-                          , "v1.0.0" ) );
-}
-
-extern "C" GLOPERATE_PLUGIN_API int getNumOfPlugins()
-{
-    return (int)g_plugins.size();
-}
-
-extern "C" GLOPERATE_PLUGIN_API gloperate::Plugin * getPlugin(unsigned int index)
-{
-    if (index < (unsigned int)g_plugins.size()) {
-        return g_plugins[index];
-    } else {
-        return nullptr;
-    }
-}
-
-extern "C" GLOPERATE_PLUGIN_API void deinitPluginLibrary()
-{
-    for (std::vector<gloperate::Plugin *>::iterator it = g_plugins.begin(); it != g_plugins.end(); ++it) {
-        gloperate::Plugin * plugin = *it;
-        delete plugin;
-    }
-    g_plugins.clear();
-}
+GLOPERATE_PLUGIN_LIBRARY_END
