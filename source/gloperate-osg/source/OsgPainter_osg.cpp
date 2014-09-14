75772056839e0f0e0c13f19d45197f76398a2a36
Stefan Buschmann <stefan.buschmann@hpi.uni-potsdam.de>
2014-09-14 16:27:05 +0200

    Fix possible memory leaks in OsgPainter

--- source/gloperate-osg/source/OsgPainter_osg.cpp
+++ source/gloperate-osg/source/OsgPainter_osg.cpp
@@ -17,13 +17,29 @@ namespace gloperate_osg
 
 void OsgPainter::osg_setScene(osg::Node * scene)
 {
+    // Release old scene
+    if (m_scene) {
+        m_scene->unref();
+    }
+
+    // Set new scene
     m_scene = scene;
     m_scene->ref();
-    m_viewer->setSceneData(m_scene);
+    if (m_viewer) {
+        m_viewer->setSceneData(m_scene);
+    }
 }
 
 void OsgPainter::osg_onInitialize()
 {
+    // Release old data
+    if (m_viewer) {
+        m_viewer->unref();
+    }
+    if (m_embedded) {
+        m_embedded->unref();
+    }
+
     // Create OSG viewer
     m_viewer = new osgViewer::Viewer;
     m_viewer->ref();
@@ -37,16 +53,22 @@ void OsgPainter::osg_onInitialize()
     m_viewer->getCamera()->setViewMatrix(osg::Matrix::lookAt(osg::Vec3(0, 0, 50), osg::Vec3(0, 0, 0), osg::Vec3(0, 1, 0))); 
 
     // Initialize viewer
+    if (m_scene) {
+        m_viewer->setSceneData(m_scene);
+    }
     m_viewer->realize();
 }
 
 void OsgPainter::osg_onPaint()
 {
-    // Send resize-event
-    m_embedded->resized(m_viewportCapability->x(), m_viewportCapability->y(), m_viewportCapability->width(), m_viewportCapability->height());
+    // Check if painter has been initialized correctly
+    if (m_viewer && m_embedded) {
+        // Send resize-event
+        m_embedded->resized(m_viewportCapability->x(), m_viewportCapability->y(), m_viewportCapability->width(), m_viewportCapability->height());
 
-    // Draw OSG scene
-    m_viewer->frame();
+        // Draw OSG scene
+        m_viewer->frame();
+    }
 }
 
 void OsgPainter::osg_cleanup()
