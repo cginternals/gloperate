1c28fef54823e05681a4b82710eb6313c9a6f8c3
Willy Scheibel <willyscheibel@gmx.de>
2014-09-30 21:09:36 +0200

    Inline StageOwned trait

--- source/gloperate/include/gloperate/pipelines/AbstractData.h
+++ source/gloperate/include/gloperate/pipelines/AbstractData.h
@@ -1,35 +1,46 @@
 #pragma once
 
-#include <vector>
+#include <string>
 
 #include <gloperate/signals/Signal.h>
 
 #include <gloperate/gloperate_api.h>
 
-#include <gloperate/pipelines/StageOwned.h>
-
 namespace gloperate {
-	
-class GLOPERATE_API AbstractData : public StageOwned
+
+class AbstractStage;
+
+class GLOPERATE_API AbstractData
 {
+    friend class AbstractStage;
 public:
-	AbstractData();
+    AbstractData(const std::string & name = "");
 	virtual ~AbstractData();
+
+    const std::string & name() const;
+    void setName(const std::string & name);
+
+    bool hasName() const;
+
+    std::string asPrintable() const;
+
+    bool hasOwner() const;
+    const AbstractStage * owner() const;
+
+    virtual std::string qualifiedName() const;
 
 	void invalidate();
 
     bool matchesName(const std::string & name) const;
 
-    const std::vector<std::string> & aliasNames() const;
-    void addAlias(const std::string & aliasName);
-    void setAliasNames(const std::vector<std::string> & aliasNames);
-
-    virtual bool isParameter() const;
-
     virtual std::string type() const = 0;
 public:
-	gloperate::Signal<> invalidated;
-    std::vector<std::string> m_aliasNames;
+    AbstractStage * m_owner;
+    std::string m_name;
+
+    gloperate::Signal<> invalidated;
+
+    void setOwner(AbstractStage * owner);
 };
 
 } // namespace gloperate
--- source/gloperate/source/pipelines/AbstractInputSlot.cpp
+++ source/gloperate/source/pipelines/AbstractInputSlot.cpp
@@ -1,9 +1,16 @@
 #include <gloperate/pipelines/AbstractInputSlot.h>
-	
+
+#include <gloperate/pipelines/AbstractStage.h>
+
+#include <sstream>
+#include <algorithm>
+
 namespace gloperate {
-	
-AbstractInputSlot::AbstractInputSlot()
-: m_hasChanged(true)
+
+AbstractInputSlot::AbstractInputSlot(const std::string & name)
+: m_owner(nullptr)
+, m_name(name)
+, m_hasChanged(true)
 , m_isOptional(false)
 , m_isFeedback(false)
 {
@@ -11,6 +18,61 @@ AbstractInputSlot::AbstractInputSlot()
 
 AbstractInputSlot::~AbstractInputSlot()
 {
+}
+
+const std::string & AbstractInputSlot::name() const
+{
+    return m_name;
+}
+
+void AbstractInputSlot::setName(const std::string & name)
+{
+    m_name = name;
+}
+
+bool AbstractInputSlot::hasName() const
+{
+    return !m_name.empty();
+}
+
+std::string AbstractInputSlot::asPrintable() const
+{
+    if (!hasName())
+        return "<unnamed>";
+
+    std::string n = name();
+    std::replace(n.begin(), n.end(), ' ', '_');
+
+    return n;
+}
+
+bool AbstractInputSlot::hasOwner() const
+{
+    return m_owner != nullptr;
+}
+
+const AbstractStage * AbstractInputSlot::owner() const
+{
+    return m_owner;
+}
+
+void AbstractInputSlot::setOwner(AbstractStage * owner)
+{
+    m_owner = owner;
+}
+
+std::string AbstractInputSlot::qualifiedName() const
+{
+    std::stringstream ss;
+
+    if (hasOwner())
+    {
+        ss << m_owner->asPrintable() << "::";
+    }
+
+    ss << asPrintable();
+
+    return ss.str();
 }
 
 bool AbstractInputSlot::isUsable() const
