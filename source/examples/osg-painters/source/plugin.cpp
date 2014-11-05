015e561e27c1e04d1db97f8190110236841e2256
Daniel.Gimbatschki <Daniel.Gimbatschki@student.hpi.uni-potsdam.de>
2014-11-05 18:01:08 +0100

    fixed compile error - adapted plugin to new structure

--- source/examples/osg-painters/source/plugin.cpp
+++ source/examples/osg-painters/source/plugin.cpp
@@ -1,13 +1,13 @@
 #include <gloperate/plugin/plugin_api.h>
-#include <osg-examples/OsgSimpleScene/OsgSimpleScene.h>
+#include <osg-painters/osg-scene/OsgScene.h>
 
 
 GLOPERATE_PLUGIN_LIBRARY
 
-    GLOPERATE_PLUGIN(OsgSimpleScene
-    , "OsgSimpleScene"
-    , "Load a simple OSG scene"
-    , "gloperate team"
-    , "v1.0.0" )
+GLOPERATE_PLUGIN(OsgScene
+, "OsgSimpleScene"
+, "Load a simple OSG scene"
+, "gloperate team"
+, "v1.0.0")
 
 GLOPERATE_PLUGIN_LIBRARY_END
