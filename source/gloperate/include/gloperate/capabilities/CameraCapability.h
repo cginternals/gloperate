09bf60e75d783c76792dfef249158732c651ad55
Jennifer Stamm <jenniferstamm1992@gmail.com>
2014-11-13 11:23:17 +0100

    Transfer Camera interface to CameraCapability

--- source/gloperate/include/gloperate/capabilities/CameraCapability.h
+++ source/gloperate/include/gloperate/capabilities/CameraCapability.h
@@ -6,10 +6,14 @@
 \******************************************************************************/
 #pragma once
 
+#include <glm/glm.hpp>
+#include <globjects/base/CachedValue.h>
+
 #include <gloperate/gloperate_api.h>
 
 #include <gloperate/capabilities/AbstractCameraCapability.h>
 
+#include <gloperate/signals/Signal.h>
 
 namespace gloperate {
 
@@ -27,7 +31,11 @@ public:
     *  @brief
     *    Constructor
     */
-    CameraCapability(AbstractViewportCapability * viewportCapability, Camera * camera = nullptr);
+    CameraCapability(AbstractViewportCapability * viewportCapability, 
+        Camera * camera = nullptr,
+        const glm::vec3 & eye = glm::vec3(0.0, 0.0, 1.0),
+        const glm::vec3 & center = glm::vec3(0.0, 0.0, 0.0),
+        const glm::vec3 & up = glm::vec3(0.0, 1.0, 0.0));
 
     /**
     *  @brief
@@ -39,10 +47,50 @@ public:
 
     virtual void setCamera(Camera * camera) override;
     virtual Camera * getCamera() override;
+    // Following is copied from Camera.h
+    bool autoUpdating() const;
+    void setAutoUpdating(bool b);
+
+    const glm::vec3 & eye() const;
+    void setEye(const glm::vec3 & eye);
+    const glm::vec3 & center() const;
+    void setCenter(const glm::vec3 & center);
+    const glm::vec3 & up() const;
+    void setUp(const glm::vec3 & up);
+
+    // lazy matrices getters
+    const glm::mat4 & view() const;
+    //const glm::mat4 & viewProjection() const;
+    const glm::mat4 & viewInverted() const;
+    //const glm::mat4 & viewProjectionInverted() const;
+
+    const glm::mat3 & normal() const;
+
+    void update() const;
+
+    void changed();
+
+
+protected:
+    void dirty(bool update = true);
+    void invalidateMatrices() const;
 
 protected:
     Camera * m_camera;
 
+    mutable bool m_dirty;
+    bool m_autoUpdate;
+
+    glm::vec3 m_eye;
+    glm::vec3 m_center;
+    glm::vec3 m_up;
+
+    globjects::CachedValue<glm::mat4> m_view;
+    globjects::CachedValue<glm::mat4> m_viewInverted;
+    globjects::CachedValue<glm::mat3> m_normal;
+
+public:
+    Signal<> invalidated;
 };
 
 
