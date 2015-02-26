a8fc0d35ef15b7e5527f0298fb70ce10f3c0d9e2
Jennifer Stamm <jenniferstamm1992@gmail.com>
2015-02-26 14:33:51 +0100

    Remove unneccessary includes

--- source/gloperate/source/painter/PipelineOutputCapability.cpp
+++ source/gloperate/source/painter/PipelineOutputCapability.cpp
@@ -2,10 +2,7 @@
 
 #include <gloperate/base/collection.hpp>
 
-#include <gloperate/pipeline/AbstractData.h>
 #include <gloperate/pipeline/AbstractPipeline.h>
-#include <gloperate/pipeline/AbstractStage.h>
-#include <gloperate/pipeline/Data.h>
 
 namespace gloperate
 {
