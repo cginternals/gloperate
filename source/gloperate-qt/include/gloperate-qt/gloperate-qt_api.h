8312f96b4fc7253d4bba458e0e29a1b142269206
Willy Scheibel <willyscheibel@gmx.de>
2015-03-31 00:24:57 +0200

    Adapt API delcarations

--- source/gloperate-qt/include/gloperate-qt/gloperate-qt_api.h
+++ source/gloperate-qt/include/gloperate-qt/gloperate-qt_api.h
@@ -1,6 +1,5 @@
 #pragma once
 
-
 // NOTE: don't export stl stuff (e.g. containers):
 // http://www.unknownroad.com/rtfm/VisualStudio/warningC4251.html
 // don't do it: http://support.microsoft.com/kb/q168958/
@@ -16,30 +15,29 @@
 #    define GLOPERATE_QT_API_IMPORT_DECLARATION
 #endif
 
-#ifndef GLOPERATE_QT_STATIC
+#ifndef GLOPERATE_STATIC
 #ifdef GLOPERATE_QT_EXPORTS
-#    define GLOPERATE_QT_API GLOPERATE_QT_API_EXPORT_DECLARATION
+#	define GLOPERATE_QT_API GLOPERATE_QT_API_EXPORT_DECLARATION
 #else
-#    define GLOPERATE_QT_API GLOPERATE_QT_API_IMPORT_DECLARATION
+#	define GLOPERATE_QT_API GLOPERATE_QT_API_IMPORT_DECLARATION
 #endif
 #else
-#    define GLOPERATE_QT_API
+#   define GLOPERATE_QT_API
 #endif
-
 
 #ifdef N_DEBUG
-#    define IF_DEBUG(statement)
-#    define IF_NDEBUG(statement) statement
+#	define IF_DEBUG(statement)
+#	define IF_NDEBUG(statement) statement
 #else
-#    define IF_DEBUG(statement) statement
-#    define IF_NDEBUG(statement)
+#	define IF_DEBUG(statement) statement
+#	define IF_NDEBUG(statement)
 #endif // N_DEBUG
 
 // http://stackoverflow.com/questions/18387640/how-to-deal-with-noexcept-in-visual-studio
 #ifndef NOEXCEPT
-#    ifdef _MSC_VER
-#        define NOEXCEPT
-#    else
-#        define NOEXCEPT noexcept
-#    endif
+#	ifdef _MSC_VER
+#		define NOEXCEPT
+#	else
+#		define NOEXCEPT noexcept
+#	endif
 #endif
