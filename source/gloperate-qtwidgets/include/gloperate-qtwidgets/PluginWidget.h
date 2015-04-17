5bc2a9c72de8a39bd1bd8c556b46016819671a09
Meike Baumgaertner <meike.baumgaertner@googlemail.com>
2015-04-17 19:21:55 +0200

    Add basic plugin show and change functionality to PluginWidget

--- source/gloperate-qtwidgets/include/gloperate-qtwidgets/PluginWidget.h
+++ source/gloperate-qtwidgets/include/gloperate-qtwidgets/PluginWidget.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <gloperate-qt/qt-includes-begin.h>
 #include <QWidget>
 #include <QScopedPointer>
+#include <gloperate-qt/qt-includes-end.h>
 
 #include <gloperate-qtwidgets/gloperate-qtwidgets_api.h>
+
+#include <memory>
 
 class Ui_PluginWidget;
 
@@ -11,6 +15,7 @@ namespace gloperate
 {
 
 class PluginManager;
+class Plugin;
 
 }
 
@@ -22,11 +27,18 @@ class GLOPERATE_QTWIDGETS_API PluginWidget : public QWidget
 	Q_OBJECT
 
 public:
-	PluginWidget(gloperate::PluginManager * pluginManager, QWidget * parent = nullptr);
+	PluginWidget(std::shared_ptr<gloperate::PluginManager> pluginManager, QWidget * parent = nullptr);
 	virtual ~PluginWidget();
 
+signals:
+    void pluginChanged(gloperate::Plugin&);
+
 protected:
-	gloperate::PluginManager * m_pluginManager;
+    virtual void initializeListView();
+    virtual void cellSelected(int nRow, int);
+
+protected:
+	std::shared_ptr<gloperate::PluginManager> m_pluginManager;
 
 private:
 	const QScopedPointer<Ui_PluginWidget> m_ui;
--- source/gloperate-qtwidgets/source/PluginWidget.cpp
+++ source/gloperate-qtwidgets/source/PluginWidget.cpp
@@ -2,23 +2,62 @@
 #include "ui_PluginWidget.h"
 
 #include <gloperate/plugin/PluginManager.h>
+#include <gloperate/plugin/Plugin.h>
 
+#include <gloperate-qt/qt-includes-begin.h>
 #include <QAbstractButton>
 #include <QWindow>
+#include <gloperate-qt/qt-includes-end.h>
+
+#include <cassert>
 
 namespace gloperate_qtwidgets
 {
 
-PluginWidget::PluginWidget(gloperate::PluginManager * pluginManager, QWidget *parent)
+PluginWidget::PluginWidget(std::shared_ptr<gloperate::PluginManager> pluginManager, QWidget *parent)
 :	QWidget(parent)
 ,	m_ui(new Ui_PluginWidget)
 ,	m_pluginManager(pluginManager)
 {
 	m_ui->setupUi(this);
+
+    initializeListView();
 }
 
 PluginWidget::~PluginWidget()
 {
+}
+
+void PluginWidget::initializeListView()
+{
+    assert(m_pluginManager);
+
+    m_ui->pluginTableWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
+    m_ui->pluginTableWidget->setRowCount(m_pluginManager->plugins().size());
+
+    QStringList tableHeader;
+    tableHeader << "Name" << "Version" << "Description" << "Type" << "Vendor";
+    m_ui->pluginTableWidget->setHorizontalHeaderLabels(tableHeader);
+
+    int pluginCount = 0;
+
+    for (auto plugin : m_pluginManager->plugins())
+    {
+        m_ui->pluginTableWidget->setItem(pluginCount, 0, new QTableWidgetItem(plugin->name()));
+        m_ui->pluginTableWidget->setItem(pluginCount, 1, new QTableWidgetItem(plugin->version()));
+        m_ui->pluginTableWidget->setItem(pluginCount, 2, new QTableWidgetItem(plugin->description()));
+        m_ui->pluginTableWidget->setItem(pluginCount, 3, new QTableWidgetItem(plugin->type()));
+        m_ui->pluginTableWidget->setItem(pluginCount++, 4, new QTableWidgetItem(plugin->vendor()));
+    }
+
+    connect(m_ui->pluginTableWidget, &QTableWidget::cellDoubleClicked,
+        this, &PluginWidget::cellSelected);
+}
+
+void PluginWidget::cellSelected(int nRow, int)
+{
+    emit pluginChanged(*m_pluginManager->plugins().at(nRow));
 }
 
 } //namespace gloperate_qtwidgets
