14bba26346de4b4273ab7435790c28b1638ae2ca
Meike Baumgaertner <meike.baumgaertner@googlemail.com>
2015-07-28 17:58:46 +0200

    Add documentation to FileNameTextEdit

--- source/gloperate-qt/include/gloperate-qt/widgets/FileNameTextEdit.h
+++ source/gloperate-qt/include/gloperate-qt/widgets/FileNameTextEdit.h
@@ -18,21 +18,72 @@ namespace gloperate_qt
 {
 
 
+/**
+*  @brief
+*    Represent ppi or pixel per cm unit and value
+*/
 class GLOPERATE_QT_API FileNameTextEdit : public QPlainTextEdit
 {
     Q_OBJECT
 
 
 public:
+    /**
+    *  @brief
+    *    Constructor
+    */
     FileNameTextEdit(QWidget *parent = 0);
+
+    /**
+    *  @brief
+    *    Destructor
+    */
     ~FileNameTextEdit();
 
+    /**
+    *  @brief
+    *    Setter of m_completer
+    *
+    *  @param[in] c
+    *    A QCompleter, null_ptr is allowed
+    */
     virtual void setCompleter(QCompleter *c);
+
+    /**
+    *  @brief
+    *    Getter of m_completer
+    *
+    *  @return
+    *    Pointer to m_completer
+    */
     virtual QCompleter *completer() const;
 
 
 protected:
+    /**
+    *  @brief
+    *    Pop completer up if former wirtten text has completions
+    *
+    *  @param[in] e
+    *    Keyevent, null_ptr is not allowed
+    *
+    *  @remarks
+    *    Custom key handling for tab, backtab, enter, escape and return if
+    *    completer is showen and for enter and return otherwise. Other keys
+    *    are executed with their default behavior.
+    */
     virtual void keyPressEvent(QKeyEvent *e);
+
+    /**
+    *  @brief
+    *    Connect completer to widget if it gets focus
+    *
+    *  @param[in] e
+    *    Focus event
+    *
+    *  @remarks
+    *    Invoke default behavior after the connection is established.
+    */
     virtual void focusInEvent(QFocusEvent *e);
 
 
