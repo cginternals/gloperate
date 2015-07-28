1bb0272ece725d258f03fdbb868ebe98af262140
Meike Baumgaertner <meike.baumgaertner@googlemail.com>
2015-07-28 18:10:26 +0200

    Add documentation to ScreenCapturerOutputWidget

--- source/gloperate-qt/include/gloperate-qt/widgets/ScreenCapturerOutputWidget.h
+++ source/gloperate-qt/include/gloperate-qt/widgets/ScreenCapturerOutputWidget.h
@@ -34,24 +34,59 @@ namespace gloperate_qt
 class QtOpenGLWindow;
 
 
+/**
+*  @brief
+*    UI for output configuration of image saving.
+*/
 class GLOPERATE_QT_API ScreenCapturerOutputWidget : public QWidget
 {
     Q_OBJECT
 
 
 public:
+    /**
+    *  @brief
+    *    Constructor
+    *
+    *  @param[in] resourceManager
+    *    Resource maanger used by viewer
+    *  @param[in] painter
+    *    Painter active in viewer
+    *  @param[in] context
+    *    OpenGl context of viewer
+    *  @param[in] screenCapturer
+    *    ScreenCapturer tool managed by UI
+    *  @param[in] parent
+    *    Parent widget
+    */
     ScreenCapturerOutputWidget(gloperate::ResourceManager & resourceManager, gloperate::Painter * painter, gloperate_qt::QtOpenGLWindow * context, gloperate::ScreenCapturer * screenCapturer, QWidget * parent = nullptr);
+    
+    /**
+    *  @brief
+    *    Destructor
+    */
     virtual ~ScreenCapturerOutputWidget();
 
 public slots:
+    /**
+    *  @brief
+    *    Update resolution summary label
+    *
+    *  @param[in] resolution
+    *    New resolution string
+    */
     void updateResolutionSummaryLabel(const std::string& resolution);
 
 
 signals:
+    /**
+    *  @brief
+    *    Emitted when filename changed
+    */
     void filenameChanged();
 
 
-protected:
+private:
     void handleSave(bool);
 
     void restoreSettings();
@@ -66,12 +101,9 @@ protected:
     void updateUiFileNameEnumIndex();
 
 
-protected:
+private:
     gloperate::ScreenCapturer * m_screenCapturer;
     gloperate_qt::QtOpenGLWindow * m_context;
-
-
-private:
     const QScopedPointer<Ui_ScreenCapturerOutputWidget> m_ui;
 };
 
