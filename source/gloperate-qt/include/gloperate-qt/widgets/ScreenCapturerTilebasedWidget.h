b598c591999a8791d1a243f9f122a5e7dda63b0c
Meike Baumgaertner <meike.baumgaertner@googlemail.com>
2015-07-28 17:38:08 +0200

    Add documentation to ScreenCapturerTilebasedWidget

--- source/gloperate-qt/include/gloperate-qt/widgets/ScreenCapturerTilebasedWidget.h
+++ source/gloperate-qt/include/gloperate-qt/widgets/ScreenCapturerTilebasedWidget.h
@@ -17,18 +17,31 @@ namespace gloperate_qt
 {
 
 
+/**
+*  @brief
+*    UI for tilebased image saving configuration. Just a dummy yet.
+*/
 class GLOPERATE_QT_API ScreenCapturerTilebasedWidget : public QWidget
 {
     Q_OBJECT
 
 
 public:
+    /**
+    *  @brief
+    *    Constructor
+    */
     ScreenCapturerTilebasedWidget(QWidget * parent = nullptr);
+
+    /**
+    *  @brief
+    *    Destructor
+    */
     virtual ~ScreenCapturerTilebasedWidget();
 
 
 private:
-    const QScopedPointer<Ui_ScreenCapturerTilebasedWidget> m_ui;
+    const QScopedPointer<Ui_ScreenCapturerTilebasedWidget> m_ui;    ///< UI of widget
 };
 
 
