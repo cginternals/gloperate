4bbe1db43f908e3f03420b22ae5dc9b6e945dfd0
Meike Baumgaertner <meike.baumgaertner@googlemail.com>
2015-07-28 17:31:16 +0200

    Add documentation to ScreenCapturer widget

--- source/gloperate-qt/include/gloperate-qt/widgets/ScreenCapturerWidget.h
+++ source/gloperate-qt/include/gloperate-qt/widgets/ScreenCapturerWidget.h
@@ -40,22 +40,46 @@ class ScreenCapturerResolutionWidget;
 class ScreenCapturerTilebasedWidget;
 
 
+/**
+*  @brief
+*    Ui for ScreenCapturer tool
+*/
 class GLOPERATE_QT_API ScreenCapturerWidget : public widgetzeug::DockableScrollAreaWidget
 {
     Q_OBJECT
 
 
 public:
+    /**
+    *  @brief
+    *    Constructor
+    *
+    *  @param[in] resourceManager
+    *    Resource maanger used by viewer
+    *  @param[in] painter
+    *    Painter currently displayed by viewer, null_ptr is allowed
+    *  @param[in] context
+    *    OpenGL context of viewer, null_ptr is not allowed
+    *
+    *  @remarks
+    *    Combine tilebased, output, resolution and datalink subwidgets to manage
+    *    ScreenCapturer tool via UI. Create ScreenCapturer instance.
+    */
     ScreenCapturerWidget(gloperate::ResourceManager & resourceManager, gloperate::Painter * painter, gloperate_qt::QtOpenGLWindow * context, QWidget * parent = nullptr);
+
+    /**
+    *  @brief
+    *    Destructor
+    */
     virtual ~ScreenCapturerWidget();
 
 
 private:
-    std::unique_ptr<ScreenCapturerOutputWidget> m_outputWidget;
-    std::unique_ptr<ScreenCapturerResolutionWidget> m_resolutionWidget;
-    std::unique_ptr<ScreenCapturerTilebasedWidget> m_tilebasedWidget;
-    std::unique_ptr<widgetzeug::DataLinkWidget> m_dataLinkWidget;
-    std::unique_ptr<gloperate::ScreenCapturer> m_screenCapturer;
+    std::unique_ptr<ScreenCapturerOutputWidget> m_outputWidget;             ///< Output subwidget
+    std::unique_ptr<ScreenCapturerResolutionWidget> m_resolutionWidget;     ///< Resolution subwidget
+    std::unique_ptr<ScreenCapturerTilebasedWidget> m_tilebasedWidget;       ///< Tilebased subwidget
+    std::unique_ptr<widgetzeug::DataLinkWidget> m_dataLinkWidget;           ///< Datalink subwidget
+    std::unique_ptr<gloperate::ScreenCapturer> m_screenCapturer;            ///< ScreenCapturer instance for the UI
 };
 
 
