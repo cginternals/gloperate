19945d97bb87dd76e62958c5332d74d540f337e8
Max Jendruk <max.jendruk@student.hpi.uni-potsdam.de>
2015-09-13 16:59:08 +0200

    Implement parseNamedStringPaths; wip parsePrograms

--- source/tools/gloperate-shader-compiler/ShaderCompiler.cpp
+++ source/tools/gloperate-shader-compiler/ShaderCompiler.cpp
@@ -1,20 +1,31 @@
 #include "ShaderCompiler.h"
 
+#include <cassert>
+
+#include <vector>
+
+#include <QJsonArray>
 #include <QJsonObject>
 #include <QOffscreenSurface>
 #include <QOpenGLContext>
 #include <QSurfaceFormat>
 
+#include <iozeug/filename.h>
+#include <iozeug/directorytraversal.h>
+
+#include <globjects/base/File.h>
+#include <globjects/NamedString.h>
 
 
 bool ShaderCompiler::process(const QJsonObject & config)
 {
+    bool ok{};
+    
     const auto jsonOpenGLConfig = config.value("opengl");
     
     if (!jsonOpenGLConfig.isObject())
         return false;
-    
-    bool ok{};
+
     const auto format = parseOpenGLFormat(jsonOpenGLConfig.toObject(), ok);
     
     if (!ok)
@@ -30,13 +41,25 @@ bool ShaderCompiler::process(const QJsonObject & config)
     surface.setFormat(context.format());
     surface.create();
     
-    context.makeCurrent(&surface);
+    ok = makeContextCurrent(context, surface,
+        [&config] ()
+        {
+            const auto jsonNamedStringPaths = config.value("namedStringPaths");
+            
+            if (!jsonNamedStringPaths.isArray())
+                return false;
+            
+            parseNamedStringPaths(jsonNamedStringPaths.toArray());
+            
+            const auto jsonPrograms = config.value("programs");
+            
+            if (!jsonPrograms.isArray())
+                return false;
+            
+            return parsePrograms(jsonPrograms.toArray());;
+        });
     
-    // TODO: Parse Named Strings and Programs
-    
-    context.doneCurrent();
-    
-    return true;
+    return ok;
 }
 
 QSurfaceFormat ShaderCompiler::parseOpenGLFormat(const QJsonObject & config, bool & ok)
@@ -89,5 +112,193 @@ QSurfaceFormat ShaderCompiler::parseOpenGLFormat(const QJsonObject & config, boo
     ok = true;
     return format;
 }
+
+bool ShaderCompiler::makeContextCurrent(
+    QOpenGLContext & context,
+    QOffscreenSurface & surface,
+    const std::function<bool ()> & functor)
+{
+    context.makeCurrent(&surface);
+    const auto ok = functor();
+    context.doneCurrent();
+    
+    return ok;
+}
+
+void ShaderCompiler::parseNamedStringPaths(const QJsonArray & paths)
+{
+    bool ok{};
+    
+    for (const auto & namedStringPath : paths)
+    {
+        if (!namedStringPath.isObject())
+            continue;
+        
+        const auto pathObject = namedStringPath.toObject();
+        
+        const auto pathString = pathObject.value("path").toString();
+        
+        if (pathString.isNull())
+            continue;
+        
+        const auto extensionsArray = pathObject.value("extensions").toArray();
+        
+        if (extensionsArray.isEmpty())
+            continue;
+        
+        const auto extensions = parseExtensions(extensionsArray, ok);
+        
+        if (!ok)
+            continue;
+        
+        auto files = scanDirectory(pathString.toStdString(), extensions);
+        
+        const auto aliasString = pathObject.value("alias").toString();
+        
+        if (aliasString.isNull())
+        {
+            createNamedStrings(files, files);
+        }
+        else
+        {
+            const auto aliases = createAliases(files,
+                pathString.toStdString(),
+                aliasString.toStdString());
+            
+            createNamedStrings(files, aliases);
+        }
+    }
+}
+
+std::set<std::string> ShaderCompiler::parseExtensions(
+    const QJsonArray & extensionsArray,
+    bool & ok)
+{
+    auto extensions = std::set<std::string>{};
+    
+    for (const auto & extensionValue : extensionsArray)
+    {
+        if (!extensionValue.isString())
+        {
+            ok = false;
+            return extensions;
+        }
+        
+        extensions.insert(extensionValue.toString().toStdString());
+    }
+    
+    ok = true;
+    return extensions;
+}
+
+std::vector<std::string> ShaderCompiler::scanDirectory(
+    const std::string & path,
+    const std::set<std::string> & extensions)
+{
+    auto files = std::vector<std::string>{};
+    
+    iozeug::scanDirectory(path, "*", true,
+        [&extensions, &files] (const std::string & fileName)
+        {
+          const auto fileExtension = iozeug::getExtension(fileName);
+          
+          if (!extensions.count(fileExtension))
+              return;
+          
+          files.push_back(fileName);
+        });
+
+    return files;
+}
+
+std::vector<std::string> ShaderCompiler::createAliases(
+    const std::vector<std::string> & files,
+    const std::string & path,
+    const std::string & alias)
+{
+    // TODO: replace path with alias
+    return files;
+}
+
+void ShaderCompiler::createNamedStrings(
+    const std::vector<std::string> & files,
+    const std::vector<std::string> & aliases)
+{
+    assert(files.size() == aliases.size());
+    
+    for (auto i = 0u; i < files.size(); ++i)
+    {
+        const auto file = files[i];
+        const auto alias = aliases[i];
+        
+        const auto fileObject = new globjects::File(file);
+        
+        globjects::NamedString::create(alias, fileObject);
+    }
+}
+
+bool ShaderCompiler::parsePrograms(const QJsonArray & programs)
+{
+    bool ok{};
+
+    for (const auto programValue : programs)
+    {
+        if (!programValue.isObject())
+            return false;
+        
+        const auto programObject = programValue.toObject();
+        
+        const auto name = programObject.value("name").toString();
+        
+        if (name.isNull())
+            return false;
+        
+        const auto shadersArray = programObject.value("shaders");
+        
+        if (!shadersArray.isArray())
+            return false;
+        
+        const auto shaders = parseShaders(shadersArray.toArray(), ok);
+        
+        if (!ok)
+            return false;
+        
+        ok = createAndLinkProgram(shaders);
+        
+        if (!ok)
+            return false;
+    }
+    
+    return true;
+}
+
+std::vector<globjects::ref_ptr<globjects::Shaders>> ShaderCompiler::parseShaders(
+    const QJsonArray & shadersArray,
+    bool & ok)
+{
+    std::vector<globjects::ref_ptr<globjects::Shaders>> shaders{};
+    
+    for (const auto & shaderValue : shadersArray)
+    {
+        if (!shaderValue.isObject())
+        {
+            ok = false;
+            return shaders;
+        }
+        
+        // TODO: continue
+    }
+    
+    ok = true;
+    return shaders;
+}
+
+bool ShaderCompiler::createAndLinkProgram(
+    const std::vector<globjects::ref_ptr<globjects::Shaders>> & shaders)
+{
+    // TODO: implement
+    return false;
+}
+
 
 
--- source/tools/gloperate-shader-compiler/ShaderCompiler.h
+++ source/tools/gloperate-shader-compiler/ShaderCompiler.h
@@ -1,15 +1,63 @@
 #pragma once
 
+#include <set>
+#include <string>
+#include <vector>
 
+#include <globjects/base/ref_ptr.h>
+
+
+class QOffscreenSurface;
+class QOpenGLContext;
+class QJsonArray;
 class QJsonObject;
 class QSurfaceFormat;
+
+namespace globjects
+{
+    class Shaders;
+}
 
 class ShaderCompiler
 {
 public:
     static bool process(const QJsonObject & config);
     
+private:
     static QSurfaceFormat parseOpenGLFormat(
         const QJsonObject & config,
         bool & ok);
+    
+    static bool makeContextCurrent(
+        QOpenGLContext & context,
+        QOffscreenSurface & surface,
+        const std::function<bool ()> & functor);
+    
+    static void parseNamedStringPaths(const QJsonArray & paths);
+    
+    static std::set<std::string> parseExtensions(
+        const QJsonArray & extensionsArray,
+        bool & ok);
+    
+    static std::vector<std::string> scanDirectory(
+        const std::string & path,
+        const std::set<std::string> & extensions);
+    
+    static std::vector<std::string> createAliases(
+        const std::vector<std::string> & files,
+        const std::string & path,
+        const std::string & alias);
+    
+    static void createNamedStrings(
+        const std::vector<std::string> & files,
+        const std::vector<std::string> & aliases);
+    
+    static bool parsePrograms(const QJsonArray & programs);
+    
+    static std::vector<globjects::ref_ptr<globjects::Shaders>> parseShaders(
+        const QJsonArray & shaders,
+        bool & ok);
+    
+    static bool createAndLinkProgram(
+        const std::vector<globjects::ref_ptr<globjects::Shaders>> & shadersArray);
 };
