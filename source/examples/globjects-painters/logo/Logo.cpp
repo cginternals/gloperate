f8397cf21791e347620fae80a35d8f5e5c6fe406
Stefan Buschmann <stefan.buschmann@hpi.de>
2015-10-07 14:59:17 +0200

    Fix data path for case of non-plugin loading

--- source/examples/globjects-painters/logo/Logo.cpp
+++ source/examples/globjects-painters/logo/Logo.cpp
@@ -70,13 +70,10 @@ Logo::Logo(ResourceManager & resourceManager, const reflectionzeug::Variant & pl
 {
     // Get data path
     std::string dataPath = "";
-    if (!pluginInfo.isNull())
-    {
-        const reflectionzeug::VariantMap & map = *(pluginInfo.asMap());
-        if (map.count("dataPath") > 0) {
-            dataPath = map.at("dataPath").value<std::string>();
-            dataPath = gloperate::ensurePathSeparatorEnding(dataPath);
-        }
+    const reflectionzeug::VariantMap * map = pluginInfo.asMap();
+    if (map && map->count("dataPath") > 0) {
+        dataPath = map->at("dataPath").value<std::string>();
+        dataPath = gloperate::ensurePathSeparatorEnding(dataPath);
     }
     m_textureFilename = dataPath + "data/logo/gloperate-logo.png";
 
