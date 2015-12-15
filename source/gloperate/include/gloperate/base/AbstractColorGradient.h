c4ebf3e1e51d2beb944204f131a3d9802ffa90c6
Willy Scheibel <scheibel@cginternals.com>
2015-12-16 00:01:42 +0100

    Add class documentation for AbstractColorGradient class

--- source/gloperate/include/gloperate/base/AbstractColorGradient.h
+++ source/gloperate/include/gloperate/base/AbstractColorGradient.h
@@ -22,11 +22,26 @@ namespace gloperate
 
 class ColorGradientList;
 
+/**
+ * @brief
+ *    The base class for color gradients (i.e., a mapping of float values in the range of [0..1] to a reflectionzeug::Color).
+ *
+ *    This class provides convenience methods for accessing the binary image data of the gradient with specific widths (pixelData)
+ *    and for using this color gradient in an OpenGL texture (generateTexture).
+ *
+ *    A color gradient is identified through its name and this name is mainly used by the ColorGradientList class.
+ *
+ *    \see ColorGradientList
+ *    \see ExplicitEquidistantColorGradient
+ */
 class GLOPERATE_API AbstractColorGradient
 {
 public:
     explicit AbstractColorGradient(const std::string & name);
 
+    /**
+     * @brief Virtual destructor for subclasses.
+     */
     virtual ~AbstractColorGradient();
 
     const std::string & name() const;
@@ -48,5 +63,6 @@ protected:
 
 
 } // namespace reflectionzeug
+
 
 #include <gloperate/base/AbstractColorGradient.hpp>
