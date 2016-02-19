4f6de6fb17d7b824818826bfd77ea16817bd4da3
Florian Wagner <florian.wagner@student.hpi.de>
2016-02-19 15:08:23 +0100

    adding interfaces for input handling

---
+++ source/gloperate/include/gloperate/input/AbstractDeviceProvider.h
@@ -0,0 +1,39 @@
+
+#pragma once
+
+#include <vector>
+#include <memory>
+
+namespace gloperate
+{
+
+class AbstractDevice;
+class InputManager;
+
+class GLOPERATE_API AbstractDeviceProvider
+{
+public:
+    /**
+     * @brief
+     *   Constructor
+     */
+    AbstractDeviceProvider() = default;
+
+    /**
+     * @brief
+     *   Destructor
+     */
+    virtual ~AbstractDeviceProvider() = default;
+
+    /**
+     *  @brief
+     *    Updates the device list and adds Devices to the Input Manager
+     */
+    virtual void updateDevices() = 0;
+
+protected:
+    InputManager * m_inputManager;
+
+};
+
+}
---
+++ source/gloperate/source/input/AbstractDevice.cpp
@@ -0,0 +1,20 @@
+
+#include <cassert>
+
+#include <gloperate/input/AbstractDevice.h>
+
+#include <gloperate/input/InputManager.h>
+
+namespace gloperate{
+
+AbstractDevice::AbstractDevice(InputManager *inputManager) :
+    m_inputManager{inputManager}
+{
+    assert(m_inputManager != nullptr);
+}
+
+AbstractDevice::~AbstractDevice()
+{
+    m_inputManager->deregisterConsumer(this);
+}
+}
