b4e93be8ea1a2bdf0143ff0e8b17f7d9a3b3d151
Florian Wagner <florian.wagner@student.hpi.de>
2016-02-27 14:54:09 +0100

    Beginning to add devices for mouse and keyboard

---
+++ source/gloperate/include/gloperate/input/MouseDevice.h
@@ -0,0 +1,15 @@
+
+#pragma once
+
+#include <gloperate/input/AbstractDevice.h>
+
+#include <gloperate/gloperate_api.h>
+
+namespace gloperate{
+
+class GLOPERATE_API MouseDevice
+{
+
+};
+
+} //namespace gloperate
