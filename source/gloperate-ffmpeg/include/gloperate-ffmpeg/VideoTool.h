f65e623d45c4d1c8d7d3a4e623e023f8c7e4b26e
Leander Neiß <leander.neiss@student.hpi.uni-potsdam.de>
2016-03-04 01:48:52 +0100

    Add comments for VideoTool.h

--- source/gloperate-ffmpeg/include/gloperate-ffmpeg/VideoTool.h
+++ source/gloperate-ffmpeg/include/gloperate-ffmpeg/VideoTool.h
@@ -28,7 +28,7 @@ namespace gloperate_ffmpeg
 
 /**
 *  @brief
-*    
+*    A tool which renders a given Stage into an output video file.
 */
 class GLOPERATE_FFMPEG_API VideoTool
 {
@@ -36,8 +36,29 @@ public:
     /**
     *  @brief
     *    Constructor
+    *
+    *  @param[in] filename
+    *    Name of output video file
+    *  @param[in] stage
+    *    The stage that will be rendered into a video
+    *  @param[in] glContext
+    *    A valid OpenGL context
+    *  @param[in] fps
+    *    Frames per second of output video
+    *  @param[in] length
+    *    Length (in seconds) of output video
+    *  @param[in] width
+    *    Width (in pixels) of output video
+    *  @param[in] height
+    *    Height (in pixels) of output video
     */
-    VideoTool(const std::string & filename, gloperate::Stage * stage, gloperate::AbstractGLContext * glContext, uint fps, uint length, uint width, uint height);
+    VideoTool(const std::string & filename,
+              gloperate::Stage * stage,
+              gloperate::AbstractGLContext * glContext,
+              uint fps,
+              uint length,
+              uint width,
+              uint height);
 
     /**
     *  @brief
@@ -45,6 +66,13 @@ public:
     */
     virtual ~VideoTool();
 
+    /**
+    *  @brief
+    *    Actual call to start video creation
+    *
+    *  @param[in] progress
+    *    Progress callback function
+    */
     void createVideo(std::function<void(int, int)> progress);
 
 
