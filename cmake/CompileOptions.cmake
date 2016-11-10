f4f59c8e1c1477e32a24588546c09efa36e90f63
Willy Scheibel <willy.scheibel@hpi.de>
2016-11-10 17:34:28 +0100

    Apply cmake-init fcfcec92f6974734b0ebb78c8ba521e9dd93ed5c

--- cmake/CompileOptions.cmake
+++ cmake/CompileOptions.cmake
@@ -19,7 +19,7 @@ endif()
 
 set(DEFAULT_PROJECT_OPTIONS
     DEBUG_POSTFIX             "d"
-    CXX_STANDARD              11
+    CXX_STANDARD              11 # Not available before CMake 3.1; see below for manual command line argument addition
     LINKER_LANGUAGE           "CXX"
     POSITION_INDEPENDENT_CODE ON
     CXX_VISIBILITY_PRESET     "hidden"
@@ -66,6 +66,7 @@ set(DEFAULT_COMPILE_OPTIONS)
 # MSVC compiler options
 if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "MSVC")
     set(DEFAULT_COMPILE_OPTIONS ${DEFAULT_COMPILE_OPTIONS}
+    PRIVATE
         /MP           # -> build with multiple processes
         /W4           # -> warning level 4
         # /WX         # -> treat warnings as errors
@@ -87,12 +88,17 @@ if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "MSVC")
         >
         
         # No manual c++11 enable for MSVC as all supported MSVC versions for cmake-init have C++11 implicitly enabled (MSVC >=2013)
+
+    PUBLIC
     )
 endif ()
 
 # GCC and Clang compiler options
 if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU" OR "${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
     set(DEFAULT_COMPILE_OPTIONS ${DEFAULT_COMPILE_OPTIONS}
+    PRIVATE
+        #-fno-exceptions # since we use stl and stl is intended to use exceptions, exceptions should not be disabled
+
         -Wall
         -Wextra
         -Wunused
@@ -121,7 +127,7 @@ if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU" OR "${CMAKE_CXX_COMPILER_ID}" MATCH
             
             -Wreturn-stack-address
         >
-        
+    PUBLIC
         $<$<PLATFORM_ID:Darwin>:
             -pthread
         >
@@ -143,6 +149,7 @@ set(DEFAULT_LINKER_OPTIONS)
 # Use pthreads on mingw and linux
 if("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU" OR "${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
     set(DEFAULT_LINKER_OPTIONS
+    PUBLIC
         -pthread
     )
 endif()
--- cmake/FindGLFW.cmake
+++ cmake/FindGLFW.cmake
@@ -1,8 +1,9 @@
 
 # GLFW_FOUND
 # GLFW_INCLUDE_DIR
-# GLFW_LIBRARY
-
+# GLFW_LIBRARY_RELEASE
+# GLFW_LIBRARY_DEBUG
+# GLFW_LIBRARIES
 # GLFW_BINARY (win32 only)
 
 include(FindPackageHandleStandardArgs)
@@ -27,6 +28,9 @@ find_path(GLFW_INCLUDE_DIR GLFW/glfw3.h
 
 find_library(GLFW_LIBRARY_RELEASE NAMES glfw3 glfw glfw3dll glfwdll
 
+    HINTS
+    ${GLFW_INCLUDE_DIR}/..
+
     PATHS
     $ENV{GLFW_DIR}
     /lib/x64
@@ -50,6 +54,9 @@ find_library(GLFW_LIBRARY_RELEASE NAMES glfw3 glfw glfw3dll glfwdll
     DOC "The GLFW library")
 
 find_library(GLFW_LIBRARY_DEBUG NAMES glfw3d glfwd glfw3ddll glfwddll
+
+    HINTS
+    ${GLFW_INCLUDE_DIR}/..
 
     PATHS
     $ENV{GLFW_DIR}
