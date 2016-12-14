0f19292ed1d09a16b7138798bb1483a20cdb5d00
Tobias Knöschke <tobias.knoeschke@student.hpi.uni-potsdam.de>
2016-12-14 11:08:24 +0100

    Adapt to changed extended properties

--- source/gloperate/include/gloperate/stages/base/CustomFramebufferStage.h
+++ source/gloperate/include/gloperate/stages/base/CustomFramebufferStage.h
@@ -11,7 +11,7 @@
 #include <globjects/Texture.h>
 
 #include <gloperate/gloperate-version.h>
-#include <gloperate/base/GlmProperties.h>
+#include <gloperate/base/ExtendedProperties.h>
 #include <gloperate/pipeline/Stage.h>
 #include <gloperate/pipeline/Input.h>
 #include <gloperate/pipeline/Output.h>
