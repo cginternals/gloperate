732396912a217ce45f121a67c99123035b7683e8
Tobias Knöschke <tobias.knoeschke@student.hpi.uni-potsdam.de>
2017-01-09 13:09:45 +0100

    Convert tabs to spaces

--- source/gloperate/source/stages/demos/LightTestPipeline.cpp
+++ source/gloperate/source/stages/demos/LightTestPipeline.cpp
@@ -43,34 +43,34 @@ LightTestPipeline::LightTestPipeline(Environment * environment, const std::strin
     });
 
     addStage(m_lightDefStage1);
-	m_lightDefStage1->color << lightColor1;
-	m_lightDefStage1->position << lightPos1;
-	m_lightDefStage1->type << lightType1;
+    m_lightDefStage1->color << lightColor1;
+    m_lightDefStage1->position << lightPos1;
+    m_lightDefStage1->type << lightType1;
     m_lightDefStage1->attenuationCoeffitients << lightAttenuation1;
     addStage(m_lightDefStage2);
-	m_lightDefStage2->color << lightColor2;
-	m_lightDefStage2->position << lightPos2;
-	m_lightDefStage2->type << lightType2;
+    m_lightDefStage2->color << lightColor2;
+    m_lightDefStage2->position << lightPos2;
+    m_lightDefStage2->type << lightType2;
     m_lightDefStage2->attenuationCoeffitients << lightAttenuation2;
-	addStage(m_lightDefStage3);
-	m_lightDefStage3->color << lightColor3;
-	m_lightDefStage3->position << lightPos3;
-	m_lightDefStage3->type << lightType3;
+    addStage(m_lightDefStage3);
+    m_lightDefStage3->color << lightColor3;
+    m_lightDefStage3->position << lightPos3;
+    m_lightDefStage3->type << lightType3;
     m_lightDefStage3->attenuationCoeffitients << lightAttenuation3;
 
     lightType1.setValue(0);
-	lightColor1.setValue(glm::vec3(0.5, 0, 0));
-	lightPos1.setValue(glm::vec3(-2, 0, -2));
+    lightColor1.setValue(glm::vec3(0.5, 0, 0));
+    lightPos1.setValue(glm::vec3(-2, 0, -2));
     lightAttenuation1.setValue(glm::vec3(1, 0.2f, 0.01f));
 
     lightType2.setValue(1);
-	lightColor2.setValue(glm::vec3(0.125, 0.125, 0.125));
-	lightPos2.setValue(glm::vec3(0, 0, 0));
+    lightColor2.setValue(glm::vec3(0.125, 0.125, 0.125));
+    lightPos2.setValue(glm::vec3(0, 0, 0));
     lightAttenuation2.setValue(glm::vec3(1, 0.2f, 0.01f));
 
     lightType3.setValue(4);
-	lightColor3.setValue(glm::vec3(1, 1, 1));
-	lightPos3.setValue(glm::vec3(1.5, -1.3, -1.5));
+    lightColor3.setValue(glm::vec3(1, 1, 1));
+    lightPos3.setValue(glm::vec3(1.5, -1.3, -1.5));
     lightAttenuation3.setValue(glm::vec3(1, 0.2f, 0.01f));
 
     addStage(m_lightAccumulationStage);
