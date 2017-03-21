fd5dfac12f2b95cdd8d8cd6ae468539dcc8bf4ac
Tobias Knöschke <tobias.knoeschke@student.hpi.uni-potsdam.de>
2017-03-21 11:56:28 +0100

    Fix Qt example compilation

--- source/examples/gloperate-qt-example/main.cpp
+++ source/examples/gloperate-qt-example/main.cpp
@@ -8,9 +8,7 @@
 #include <gloperate/gloperate.h>
 #include <gloperate/base/Environment.h>
 #include <gloperate/base/GLContextUtils.h>
-
-#include <demo-stages/DemoStage.h>
-#include <demo-stages/DemoPipeline.h>
+#include <gloperate/pipeline/Stage.h>
 
 #include <gloperate-qt/base/GLContext.h>
 #include <gloperate-qt/base/Application.h>
@@ -44,7 +42,7 @@ int main(int argc, char * argv[])
     UpdateManager updateManager(&environment);
 
     // Create render stage
-    auto renderStage = cppassist::make_unique<gloperate::DemoStage>(&environment);
+    auto renderStage = environment.componentManager()->component<Stage>("DemoStage")->createInstance(&environment);
 
     // Create render window
     auto window = cppassist::make_unique<RenderWindow>(&environment);
