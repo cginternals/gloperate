c7fcd1b7502dc6346bebaee0dedb0afa2d4f9be4
Christopher <christopher.weyand@student.hpi.de>
2017-04-03 16:10:15 +0200

    Comments for CameraUtils

--- source/gloperate/include/gloperate/rendering/CameraUtils.h
+++ source/gloperate/include/gloperate/rendering/CameraUtils.h
@@ -1,6 +1,29 @@
 
 #include <glm/mat4x4.hpp>
 
+/**
+*  @brief
+*    Compute orthographic projection matrix from perspective parameters
+*
+*  @param[in] fovy, aspectRatio, zNear, zFar
+*    same as for glm::perspective
+*
+* @param[in] syncDist
+*    distance of the plane that should keep constant size in both projections
+*    range: [zNear, zFar]
+*/
 glm::mat4 orthographicFromPerspective(float fovy, float aspectRatio, float zNear, float zFar, float syncDist);
 
+/**
+*  @brief
+*    Compute perspective projection matrix from orthographic parameters
+*
+*  @param[in] left, right, bottom, top, zNear, zFar
+*    same as for glm::ortho
+*    left = -right; bottom = -top
+*
+* @param[in] syncDist
+*    distance of the plane that should keep constant size in both projections
+*    range: [zNear, zFar]
+*/
 glm::mat4 perspectiveFromOrthographic(float left, float right, float bottom, float top, float zNear, float zFar, float syncDist);
