8427adba54e5326fb489261d81114f855d089cdd
Willy Scheibel <willy.scheibel@hpi.de>
2017-05-12 15:07:44 +0200

    Add KernelToPointInPlaneStage and CubeMapProjectionStage

---
+++ source/gloperate/include/gloperate/stages/base/CubeMapProjectionsStage.h
@@ -0,0 +1,44 @@
+
+#pragma once
+
+#include <vector>
+
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <gloperate/pipeline/Stage.h>
+#include <gloperate/pipeline/Output.h>
+#include <gloperate/pipeline/Input.h>
+
+#include <gloperate/gloperate_api.h>
+
+
+namespace gloperate
+{
+
+class GLOPERATE_API CubeMapProjectionsStage : public gloperate::Stage
+{
+public:
+    CubeMapProjectionsStage(gloperate::Environment * environment, const std::string & name = "CubeMapProjectionsStage");
+    virtual void onContextInit(gloperate::AbstractGLContext * context) override;
+
+
+public:
+    Input<glm::vec3> center;
+    Input<float>     nearPlane;
+    Input<float>     farPlane;
+
+    Output<std::vector<glm::mat4> *> projections;
+
+
+protected:
+    virtual void onProcess(gloperate::AbstractGLContext * context) override;
+
+
+protected:
+    std::vector<glm::mat4> m_projections;
+};
+
+
+} // namespace gloperate
---
+++ source/gloperate/source/stages/base/CubeMapProjectionsStage.cpp
@@ -0,0 +1,47 @@
+#include <gloperate/stages/base/CubeMapProjectionsStage.h>
+
+#include <glm/gtc/matrix_transform.hpp>
+
+
+namespace
+{
+
+const glm::vec3 viewDirs[] = { glm::vec3(1, 0, 0), glm::vec3(-1, 0, 0), glm::vec3(0, 1, 0), glm::vec3(0, -1, 0), glm::vec3(0, 0, 1), glm::vec3(0, 0, -1) };
+const glm::vec3 ups[] = { glm::vec3(0, -1, 0), glm::vec3(0, -1, 0), glm::vec3(0, 0, 1), glm::vec3(0, 0, -1), glm::vec3(0, -1, 0), glm::vec3(0, -1, 0) };
+
+} // namespace
+
+
+namespace gloperate
+{
+
+CubeMapProjectionsStage::CubeMapProjectionsStage(gloperate::Environment * environment, const std::string & name)
+: Stage(environment, name)
+, center("center", this, glm::vec3(0.0f, 0.0f, 0.0f))
+, nearPlane("nearPlane", this, 0.1f)
+, farPlane("farPlane", this, 4.0f)
+, projections("projections", this, nullptr)
+{
+    m_projections.resize(6);
+}
+
+void CubeMapProjectionsStage::onContextInit(gloperate::AbstractGLContext * /*context*/)
+{
+}
+
+void CubeMapProjectionsStage::onProcess(gloperate::AbstractGLContext * /*context*/)
+{
+    const auto c = *center;
+
+    for (int i = 0; i < 6; ++i)
+    {
+        const auto projection = glm::perspective(glm::radians(90.0f), 1.0f, *nearPlane, *farPlane);
+        const auto view = glm::lookAt(c, c + viewDirs[i], ups[i]);
+
+        projections->at(i) = std::move(projection) * std::move(view);
+    }
+
+    projections.setValue(&m_projections);
+}
+
+} // namespace gloperate
