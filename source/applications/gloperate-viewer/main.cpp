f05e905a9993d7af224b1388528302bf5a9014c2
Stefan Buschmann <stefan.buschmann@hpi.de>
2017-05-14 00:18:14 +0200

    Create gloperate_qtquick::Application that takes care of initializing gloperate to avoid code duplication in examples

--- source/applications/gloperate-viewer/main.cpp
+++ source/applications/gloperate-viewer/main.cpp
@@ -1,85 +1,23 @@
 
-#include <QQmlContext>
-#include <QSurfaceFormat>
-
-#include <cppassist/cmdline/ArgumentParser.h>
-
-#include <cppexpose/scripting/ScriptContext.h>
-#include <cppexpose/reflection/Property.h>
-
-#include <qmltoolbox/MessageHandler.h>
-
-#include <gloperate/gloperate.h>
 #include <gloperate/gloperate-version.h>
-#include <gloperate/base/Environment.h>
 
-#include <gloperate-qt/base/GLContext.h>
-#include <gloperate-qt/base/GLContextFactory.h>
-#include <gloperate-qt/base/UpdateManager.h>
-
-#include <gloperate-qtquick/QmlEngine.h>
-#include <gloperate-qtquick/QmlScriptContext.h>
-
-#include "Application.h"
+#include <gloperate-qtquick/Application.h>
 
 
-using namespace gloperate;
-using namespace gloperate_qt;
 using namespace gloperate_qtquick;
 
 
 int main(int argc, char * argv[])
 {
-    // Redirect output
-    qmltoolbox::MessageHandler::instance().installMessageHandlers();
-
-    // Read command line options
-    cppassist::ArgumentParser argumentParser;
-    argumentParser.parse(argc, argv);
-
-    const auto contextString = argumentParser.value("--context");
-
-    // Create gloperate environment
-    Environment environment;
-
     // Initialize Qt application
-    ::Application app(&environment, argc, argv);
-
-    // Configure update manager
-    UpdateManager updateManager(&environment);
-
-    // Create QML engine
-    QmlEngine qmlEngine(&environment);
-
-    // Create scripting context backend
-    environment.setupScripting(
-        cppassist::make_unique<gloperate_qtquick::QmlScriptContext>(&qmlEngine)
-    );
-
-    // Configure and load plugins
-    environment.componentManager()->addPluginPath(
-        gloperate::pluginPath(), cppexpose::PluginPathType::Internal
-    );
-    environment.componentManager()->scanPlugins("loaders");
-    environment.componentManager()->scanPlugins("stages");
-    environment.componentManager()->scanPlugins("exporter");
-
-    // Specify desired context format
-    gloperate::GLContextFormat format;
-
-    if (!contextString.empty())
-    {
-        if (!format.initializeFromString(contextString))
-        {
-            return 1;
-        }
-    }
-
-    QSurfaceFormat qFormat = gloperate_qt::GLContextFactory::toQSurfaceFormat(format);
-    QSurfaceFormat::setDefaultFormat(qFormat);
+    Application app(argc, argv);
+    app.setApplicationName(GLOPERATE_PROJECT_NAME);
+    app.setApplicationVersion(GLOPERATE_VERSION);
+    app.setOrganizationName(GLOPERATE_AUTHOR_ORGANIZATION);
+    app.setOrganizationDomain(GLOPERATE_AUTHOR_DOMAIN);
 
     // Load and show QML
-    qmlEngine.load(QUrl::fromLocalFile(qmlEngine.gloperateModulePath() + "/Viewer.qml"));
+    app.loadQml(app.gloperateModulePath() + "/Viewer.qml");
 
     // Run main loop
     return app.exec();
