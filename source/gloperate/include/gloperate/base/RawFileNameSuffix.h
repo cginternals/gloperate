1cc9bbcaa26f1b099fa35abe2b436a7a7077cbf1
Stefan Buschmann <stefan.buschmann@hpi.de>
2017-05-31 16:09:27 +0200

    Fix comments and style

--- source/gloperate/include/gloperate/base/RawFileNameSuffix.h
+++ source/gloperate/include/gloperate/base/RawFileNameSuffix.h
@@ -1,4 +1,6 @@
+
 #pragma once
+
 
 #include <string>
 
@@ -14,7 +16,6 @@ namespace gloperate
 /**
 *  @brief
 *    Class to parse the filename of .raw files and extract the attributes of the texture.
-*
 */
 class GLOPERATE_API RawFileNameSuffix
 {
@@ -28,75 +29,95 @@ public:
     */
     RawFileNameSuffix(const std::string & fileName);
 
-
     /**
-    * @brief
-    *   Return true, if width, height, type, and format if not compressed have valid values.
+    *  @brief
+    *    Check if filename is valid
+    *
+    *  @return
+    *    Return true, if width, height, type, and format if not compressed have valid values.
     */
     bool isValid() const;
 
     /**
-    * @brief
-    *   Return width of raw texture file name suffix in pixel;
-    *   Is -1 when no width is provided.
+    *  @brief
+    *    Get texture with
+    *
+    *  @return
+    *    Width of raw texture file name suffix in pixels, -1 when no width is provided.
     */
     int width()  const;
 
     /**
-    * @brief
-    *   Return height of raw texture file name suffix in pixel;
-    *   Is -1 when no height is provided.
+    *  @brief
+    *    Get texture height
+    *
+    *  @return
+    *    Height of raw texture file name suffix in pixels, -1 when no height is provided.
     */
     int height() const;
 
     /**
-    * @brief
-    *   Returns type of raw texture file name suffix;
-    *   Is GL_NONE when type is not unknown or not provided.
+    *  @brief
+    *    Check if texture is compressed
+    *
+    *  @return
+    *    'true' if texture type is compressed and no extra format is given, 'false' otherwise.
+    *    When type is of GL_NONE, returns false.
+    */
+    bool compressed() const;
+
+    /**
+    *  @brief
+    *    Get texture type
+    *
+    *  @return
+    *    Type of raw texture file name suffix, GL_NONE when type is not unknown or not provided.
     */
     gl::GLenum type() const;
 
     /**
-    * @brief
-    *   Returns format of raw texture file name suffix;
-    *   Is GL_NONE when format is not unknown or not provided.
+    *  @brief
+    *    Get texture format
+    *
+    *  @return
+    *    Format of raw texture file name suffix, GL_NONE when format is not unknown or not provided.
     */
     gl::GLenum format() const;
 
-    /**
-    * @brief
-    *   Returns true if texture type is compressed and no extra format is given; false otherwise.
-    *   When type is of GL_NONE, returns false.
-    */
-    bool compressed() const;
 
 protected:
     /**
     *  @brief
-    *    find format enum that corresponds to suffix
+    *    Find format enum that corresponds to suffix
     *
     *  @param[in] suffix
     *    part of a .raw filename
+    *
+    *  @return
+    *    OpenGL format
     */
     static gl::GLenum format(const std::string & suffix);
 
     /**
     *  @brief
-    *    find type enum that corresponds to suffix
+    *    Find type enum that corresponds to suffix
     *
     *  @param[in] suffix
     *    part of a .raw filename
+    *
+    *  @return
+    *    OpenGL format
     */
     static gl::GLenum type(const std::string & suffix);
 
+
 protected:
-    int m_width;  ///< Width of the Texture
-    int m_height; ///< Height of the Texture
-
-    gl::GLenum m_format; ///< Format of the Texture
-    gl::GLenum m_type;   ///< Type of the Texture
-
-    bool m_compressed;   ///< states if Texture is compressed or not
+    int        m_width;      ///< Width of the Texture
+    int        m_height;     ///< Height of the Texture
+    bool       m_compressed; ///< states if Texture is compressed or not
+    gl::GLenum m_format;     ///< Format of the Texture
+    gl::GLenum m_type;       ///< Type of the Texture
 };
+
 
 } // namespace gloperate
