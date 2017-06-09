ea6b8853479750317881697400adf04c1fe187ed
Stefan Buschmann <stefan.buschmann@hpi.de>
2017-06-09 14:50:17 +0200

    Add trackball camera navigation stage

---
+++ source/gloperate/include/gloperate/stages/navigation/TrackballStage.h
@@ -0,0 +1,84 @@
+
+#pragma once
+
+
+#include <cppexpose/plugin/plugin_api.h>
+
+#include <gloperate/gloperate-version.h>
+#include <gloperate/pipeline/Stage.h>
+#include <gloperate/pipeline/Input.h>
+#include <gloperate/pipeline/Output.h>
+#include <gloperate/input/AbstractEventConsumer.h>
+#include <gloperate/rendering/Camera.h>
+
+
+namespace gloperate
+{
+
+
+/**
+*  @brief
+*    Stage that implements a trackball camera navigation
+*/
+class GLOPERATE_API TrackballStage : public gloperate::Stage, gloperate::AbstractEventConsumer
+{
+public:
+    CPPEXPOSE_DECLARE_COMPONENT(
+        TrackballStage, gloperate::Stage
+      , "NavigationStage" // Tags
+      , "" // Icon
+      , "" // Annotations
+      , "Stage that implements a trackball camera navigation"
+      , GLOPERATE_AUTHOR_ORGANIZATION
+      , "v0.1.0"
+    )
+
+
+public:
+    // Inputs
+    Input<glm::vec4> viewport; ///< Viewport (in real device coordinates)
+
+    // Outputs
+    Output<gloperate::Camera *> camera; ///< Camera for the scene
+
+
+public:
+    /**
+    *  @brief
+    *    Constructor
+    *
+    *  @param[in] environment
+    *    Environment to which the stage belongs (must NOT be null!)
+    *  @param[in] name
+    *    Stage name
+    */
+    TrackballStage(gloperate::Environment * environment, const std::string & name = "TrackballStage");
+
+    /**
+    * @brief
+    *   Destructor
+    */
+    ~TrackballStage();
+
+
+protected:
+    // Virtual AbstractEventConsumer interface
+    virtual void onEvent(gloperate::InputEvent * event) override;
+
+    // Virtual Stage interface
+    virtual void onProcess() override;
+
+
+protected:
+    std::unique_ptr<gloperate::Camera> m_camera; ///< Camera that is controlled by the stage
+
+    glm::ivec2 m_lastMousePosition; ///< Last known mouse position
+    bool       m_dragging;          ///< 'true' if a dragging operation is currently active, else 'false'
+
+    float m_pitch; ///< Current rotation around the X axis
+    float m_yaw;   ///< Current rotation around the Y axis
+    float m_zoom;  ///< Current zoom factor
+};
+
+
+} // namespace gloperate
---
+++ source/gloperate/source/stages/navigation/TrackballStage.cpp
@@ -0,0 +1,107 @@
+
+#include <gloperate/stages/navigation/TrackballStage.h>
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/euler_angles.hpp>
+
+#include <cppassist/memory/make_unique.h>
+
+#include <gloperate/base/Environment.h>
+#include <gloperate/input/MouseEvent.h>
+
+
+namespace gloperate
+{
+
+
+CPPEXPOSE_COMPONENT(TrackballStage, gloperate::Stage)
+
+
+TrackballStage::TrackballStage(Environment * environment, const std::string & name)
+: Stage(environment, name), AbstractEventConsumer(environment->inputManager())
+, viewport("viewport", this)
+, camera("camera", this)
+, m_camera(cppassist::make_unique<Camera>())
+, m_lastMousePosition(0, 0)
+, m_dragging(false)
+, m_pitch(0.f)
+, m_yaw(0.f)
+, m_zoom(1.f)
+{
+}
+
+TrackballStage::~TrackballStage()
+{
+}
+
+void TrackballStage::onEvent(InputEvent * event)
+{
+    switch(event->type())
+    {
+        case InputEvent::Type::MouseButtonPress:
+            if (static_cast<MouseEvent*>(event)->button() == MouseButtonLeft)
+            {
+                // Start dragging
+                assert(!m_dragging);
+                m_lastMousePosition = static_cast<MouseEvent*>(event)->pos();
+                m_dragging = true;
+            }
+            break;
+
+        case InputEvent::Type::MouseButtonRelease:
+            if (static_cast<MouseEvent*>(event)->button() == MouseButtonLeft)
+            {
+                // Stop dragging
+                assert(m_dragging);
+                m_dragging = false;
+            }
+            break;
+
+        case InputEvent::Type::MouseMove:
+            // Rotation
+            if (m_dragging)
+            {
+                auto currentMousePosition = static_cast<MouseEvent*>(event)->pos();
+                float scaling = 0.15f;
+
+                m_pitch += (m_lastMousePosition.y - currentMousePosition.y) * scaling;
+                m_pitch = glm::clamp(m_pitch, -89.f, 89.f);
+
+                m_yaw += (m_lastMousePosition.x - currentMousePosition.x) *scaling;
+                m_yaw = m_yaw -180 % 360 + 180;
+
+                m_lastMousePosition = currentMousePosition;
+                camera.invalidate();
+            }
+            break;
+
+        case InputEvent::Type::MouseWheelScroll:
+            // Zooming
+            if (!m_dragging)
+            {
+                m_zoom -= static_cast<MouseEvent*>(event)->wheelDelta().y / 3000;
+                m_zoom = glm::clamp(m_zoom,0.4f,1.5f);
+
+                camera.invalidate();
+            }
+            break;
+
+        default:
+            break;
+    }
+}
+
+void TrackballStage::onProcess()
+{
+    m_camera->setAspectRatio(viewport->z / viewport->w);
+
+    auto mat = glm::yawPitchRoll(glm::radians(m_yaw), glm::radians(m_pitch), 0.f);
+    glm::vec4 rotatedCameraPosition = mat * glm::vec4(0.f, 0.f, 3.f * m_zoom, 1.f);
+
+    m_camera->setEye(glm::vec3(rotatedCameraPosition) / rotatedCameraPosition.w);
+
+    camera.setValue(m_camera.get());
+}
+
+
+} // namespace gloperate
