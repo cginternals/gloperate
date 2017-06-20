ee95c81eb0c57c273eb064b81e24cda5e422f0de
Tobias Knöschke <tobias.knoeschke@student.hpi.uni-potsdam.de>
2017-06-20 15:04:57 +0200

    Copy & adapt assimp mesh loader from gloperate 1.0

---
+++ source/examples/demo-stages-plugins/AssimpMeshLoader.h
@@ -0,0 +1,58 @@
+
+#pragma once
+
+
+#include <gloperate/base/Loader.h>
+
+
+struct aiMesh;
+struct aiScene;
+
+namespace gloperate
+{
+    class Drawable;
+}
+
+
+/**
+*  @brief
+*    Loader for meshes (PolygonalGeometry) that uses ASSIMP for import
+*
+*  Supported options:
+*    "smoothNormals" <bool>: Generate smooth normals
+*/
+class AssimpMeshLoader : public gloperate::Loader<gloperate::Drawable>
+{
+public:
+    /**
+    *  @brief
+    *    Constructor
+    */
+    AssimpMeshLoader(gloperate::Environment * environment);
+
+    /**
+    *  @brief
+    *    Destructor
+    */
+    virtual ~AssimpMeshLoader();
+
+    // Virtual loader methods
+    virtual bool canLoad(const std::string & ext) const override;
+    virtual std::vector<std::string> loadingTypes() const override;
+    virtual std::string allLoadingTypes() const override;
+    virtual gloperate::Drawable * load(const std::string & filename, const cppexpose::Variant & options, std::function<void(int, int)> progress) const override;
+
+
+protected:
+    /**
+    *  @brief
+    *    Convert ASSIMP mesh into gloperate mesh
+    *
+    *  @param[in] mesh
+    *    ASSIMP mesh (must be valid!)
+    *
+    *  @return
+    *    Mesh, must be destroyed by the caller
+    */
+    gloperate::Drawable * convertGeometry(const aiMesh * mesh) const;
+};
